fcc2561acfb8ba54c7d15330e16b299cacb8546e
nourinfathima <nourinnourin36@gmail.com>
2022-11-06 23:40:35 +0530

    add

--- src/employe.java
+++ src/employe.java
@@ -1,25 +1,68 @@
+import java.sql.*;
 import java.util.Scanner;
 
 public class employe {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int choice, empcode, empsalary;
+        String empname, empdes, compname, empemail, emppassword, empphone;
 
 
-    int choice;
-    while (true) {
-        System.out.println("select an option");
-        System.out.println("1.insert");
-        System.out.println("2.view");
-        System.out.println("3.search");
-        System.out.println("4.delete");
-        System.out.println("5.update");
-        System.out.println("6.Exit");
-        Scanner scanner = new Scanner(System.in);
-        choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("insert recipes selected");
-                break;
-            case 2:
+
+        while(true){
+            System.out.println("Employee Management App");
+            System.out.println("1. Add Employee");
+            System.out.println("2. View All Employees");
+            System.out.println("3. Search for a Employee");
+            System.out.println("4. Update a Employee");
+            System.out.println("5. Delete a Employee");
+            System.out.println("6. Exit ");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Add Employee");
+                    System.out.println("Enter the Employee code: ");
+                    empcode = input.nextInt();
+                    System.out.println("Enter Employee name: ");
+                    empname = input.next();
+                    System.out.println("Enter the designation: ");
+                    empdes = input.next();
+                    System.out.println("Enter the salary: ");
+                    empsalary = input.nextInt();
+                    System.out.println("Enter the Company name: ");
+                    compname = input.next();
+                    System.out.println("Enter the Phone Number: ");
+                    empphone = input.next();
+                    System.out.println("Enter the Email Id: ");
+                    empemail = input.next();
+                    System.out.println("Enter the password: ");
+                    emppassword = input.next();
+
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root","");
+                        String sql = "INSERT INTO `employees`(`empcode`, `empname`, `designation`, `salary`, `companyname`, `phone`, `emailid`, `password`) VALUES (?,?,?,?,?,?,?,?)";
+                        PreparedStatement stmt = con.prepareStatement(sql);
+                        stmt.setInt(1,empcode);
+                        stmt.setString(2,empname);
+                        stmt.setString(3,empdes);
+                        stmt.setInt(4,empsalary);
+                        stmt.setString(5,compname);
+                        stmt.setString(6,empphone);
+                        stmt.setString(7,empemail);
+                        stmt.setString(8,emppassword);
+                        stmt.executeUpdate();
+
+
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
+                    break;
+
+                case 2:
                 System.out.println("view selected");
                 break;
             case 3:
@@ -43,4 +86,3 @@ public class employe {
     }
 }
 }
-
